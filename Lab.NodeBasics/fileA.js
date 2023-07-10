09d113e7505cf562d2974440afb277a1ec032812
Mario Carbache <mariocarbache@gmail.com>
2023-07-10 13:43:58 -0400

    Node.js solo lab

---
+++ Lab.NodeBasics/fileA.js
@@ -0,0 +1,6 @@
+const exportsSnippet1 = require('./snippet1');
+const exportsSnippet2 = require('./snippet2');
+const exportsSnippet3 = require('./snippet3');
+console.log(exportsSnippet1)
+console.log(exportsSnippet2)
+console.log(exportsSnippet3)
